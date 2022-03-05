cc6e723db38f650f68800cc713d52f180ce10c5f
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2022-03-05 19:30:11 +0900

    Modify MVC
    MVC 패턴으로 수정.
    
    MainModel - Model
    MainView - View
    MainActivity - Controller

--- designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
+++ designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
@@ -1,42 +1,25 @@
 package com.example.designPattern;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+// Controller
 public class MainActivity extends AppCompatActivity {
 
-    private String saveData;
-
-    private EditText edtInput;
-    private Button btnSave, btnLoad;
-    private TextView txtOutput;
+    private MainModel model;
+    private MainView mView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtInput = findViewById(R.id.edtInput);
-        btnSave = findViewById(R.id.btnSave);
-        btnLoad = findViewById(R.id.btnLoad);
-        txtOutput = findViewById(R.id.txtOutput);
+        model = new MainModel();
+        mView = new MainView(this);
 
-        btnSave.setOnClickListener(view -> saveData = getInputData());
-        btnLoad.setOnClickListener(view -> showData(saveData));
-    }
-
-    @NonNull
-    private String getInputData() {
-        return edtInput.getText().toString();
-    }
-
-    private void showData(String data) {
-        txtOutput.setText(data);
+        mView.getBtnSave().setOnClickListener(view -> model.setData(mView.getInputData()));
+        mView.getBtnLoad().setOnClickListener(view -> mView.showData(model.getData()));
     }
 
 }
---
+++ designPattern/app/src/main/java/com/example/designPattern/MainModel.java
@@ -0,0 +1,18 @@
+package com.example.designPattern;
+
+// Model
+public class MainModel {
+
+    private String data;
+
+    public MainModel() {
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
