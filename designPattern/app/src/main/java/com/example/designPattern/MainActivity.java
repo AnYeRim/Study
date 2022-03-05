65e4268aa08f7dcdad3966709a1c5d2edd5f8a9c
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2022-03-05 23:49:04 +0900

    Add More Features
    간단한 코드를 mvc mvp로 수정해보니까 얼마나 효율적인 것인지 잘 안 느껴진다.
    
    더 기능을 추가해서 늘어난 코드로 비교해보자

--- designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
+++ designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
@@ -2,41 +2,76 @@ package com.example.designPattern;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
-    private String saveData;
+    private String name;
+    private Date birthday;
 
-    private EditText edtInput;
+    private EditText edtName, edtBirthday;
     private Button btnSave, btnLoad;
-    private TextView txtOutput;
+    private TextView txtName, txtBirthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtInput = findViewById(R.id.edtInput);
+        edtName = findViewById(R.id.edtName);
+        edtBirthday = findViewById(R.id.edtBirthday);
         btnSave = findViewById(R.id.btnSave);
         btnLoad = findViewById(R.id.btnLoad);
-        txtOutput = findViewById(R.id.txtOutput);
+        txtName = findViewById(R.id.txtName);
+        txtBirthday = findViewById(R.id.txtBirthday);
 
-        btnSave.setOnClickListener(view -> saveData = getInputData());
-        btnLoad.setOnClickListener(view -> showData(saveData));
+        btnSave.setOnClickListener(view -> {
+            name = getName();
+            birthday = toDate(getBirthday());
+        });
+        btnLoad.setOnClickListener(view -> {
+            showData(name, toString(birthday));
+        });
     }
 
     @NonNull
-    private String getInputData() {
-        return edtInput.getText().toString();
+    private String getBirthday() {
+        return edtBirthday.getText().toString();
     }
 
-    private void showData(String data) {
-        txtOutput.setText(data);
+    private Date toDate(String date) {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+            return dateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @NonNull
+    private String toString(Date date) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+        return dateFormat.format(date);
+    }
+
+    @NonNull
+    private String getName() {
+        return edtName.getText().toString();
+    }
+
+    private void showData(String name, String birthday) {
+        txtName.setText(name);
+        txtBirthday.setText(birthday);
     }
 
 }
