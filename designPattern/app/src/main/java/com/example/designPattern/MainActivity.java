584234bb5135c27600ca21ddae9ec7f646907b2b
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2022-03-05 20:38:12 +0900

    Modify MVC 2
    이전에 수정한 버전이 mvc가 아니라고 한다.
    
    Model : MainModel
    View : activity_main.xml
    Controller : MainActivity
    
    안드로이드에는 mvc 적용해도 Controller에서 View 셋팅도 함께 되기때문에 Controller에만 일이 몰린다.
    
    그래서 mvp가 나왔다.
    MainActivity에서는 View 관련된 것만 하고, Controller가 하는 일을 Presenter이라는 이름으로 따로 뺀다.
    
    (이해 안 되는 것 : mvp에서는 Presenter은 따로 빼면서 왜 mvc에서는 v와 c를 굳이 분류하지 않는가..? 분류하는게 더 번거로운 작업이 되는건가..?)

--- designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
+++ designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.designPattern;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-// Controller
+// Controller : model 과 view 의 중간다리 역할.
+// 안드로이드에서 view = xml
+// 사용자 요청이 들어오면 관련된 작업 처리 (모델에 데이터 저장 및 호출, 뷰에 출력 요청)
 public class MainActivity extends AppCompatActivity {
 
     private MainModel model;
-    private MainView mView;
+
+    private EditText edtInput;
+    private Button btnSave, btnLoad;
+    private TextView txtOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +25,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         model = new MainModel();
-        mView = new MainView(this);
 
-        mView.getBtnSave().setOnClickListener(view -> model.setData(mView.getInputData()));
-        mView.getBtnLoad().setOnClickListener(view -> mView.showData(model.getData()));
+        edtInput = findViewById(R.id.edtInput);
+        btnSave = findViewById(R.id.btnSave);
+        btnLoad = findViewById(R.id.btnLoad);
+        txtOutput = findViewById(R.id.txtOutput);
+
+        btnSave.setOnClickListener(view -> model.setData(getInputData()));
+        btnLoad.setOnClickListener(view -> showData(model.getData()));
+    }
+
+    @NonNull
+    public String getInputData() {
+        return edtInput.getText().toString();
+    }
+
+    public void showData(String data) {
+        txtOutput.setText(data);
     }
 
 }
