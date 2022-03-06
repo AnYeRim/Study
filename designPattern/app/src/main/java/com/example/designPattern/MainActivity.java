dd756eca1aeb1dc56e4ddc2e53271b43edb5c938
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2022-03-06 16:35:44 +0900

    Revert "Modify MVVM 2"
    This reverts commit 3881ff7cace895b08b3292e1cb9ea683215a313d.

--- designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
+++ designPattern/app/src/main/java/com/example/designPattern/MainActivity.java
@@ -22,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
         viewModel = new MainViewModel(this);
         binding.setViewModel(viewModel);
+
+        binding.btnSave.setOnClickListener(view -> viewModel.onClickBtnSave());
+        binding.btnLoad.setOnClickListener(view -> viewModel.onClickBtnLoad());
     }
 
     @NonNull
