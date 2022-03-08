b719e28d7ccbd1266617b8bc9134ae8eae92528e
AnYeRim <49059414+AnYeRim@users.noreply.github.com>
2022-03-08 15:52:30 +0900

    Practice Writing Test Code
    테스트 코드 작성하는 것에 대해 연습
    
    MVVM말고 MVP, MVC부터 연습해야겠다..
    
    MVVM에 LiveData의 setValue부분에서 오류가 나 진행이 안된다ㅠㅠ
    
    setValue는 메인스레드에서 작동하기 때문에, 백그라운드에서 실행하면 오류가 난다는 것이다.
    
    이 경우 InstantTaskExecutorRule를 사용하면 내부에 isMainThread()가 true로 코딩되어있어 해결된다는데.. 난 해결되지 않는다.
    
    이부분은 추후 다시 고민하자

---
+++ designPattern/app/src/test/java/com/example/designPattern/MainViewModelTest.java
@@ -0,0 +1,37 @@
+package com.example.designPattern;
+
+import static org.junit.Assert.*;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainViewModelTest {
+
+    @Rule
+    public InstantTaskExecutorRule rule = new InstantTaskExecutorRule();
+
+    MainViewModel viewModel;
+
+    @Before
+    public void setUp() {
+        if (viewModel == null)
+            viewModel = new MainViewModel();
+    }
+
+    @Test
+    public void getName() {
+        viewModel.onClickBtnSave("a", "19970701");
+        assertEquals(null, viewModel.getName().getValue());
+
+        viewModel.onClickBtnLoad();
+        assertEquals(null, viewModel.getName().getValue());
+
+    }
+
+    @Test
+    public void getBirthday() {
+    }
+}
