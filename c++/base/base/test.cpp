66a3e8859399163403e9c75c7dc9088d5b65a7b8
yran <tmvksals@naver.com>
2022-12-12 18:52:01 +0900

    c++ / printf

--- c++/base/base/test.cpp
+++ c++/base/base/test.cpp
@@ -1,9 +1,37 @@
 #include <stdio.h>
 
-int main(){
-	int a = 1;
+void printA(){
+	int num = 65;
+	char ch = 'A';
 
-	printf("int a print : %d\n", a);
+	printf("num : %d, ch : %c\n", num, ch);
+	printf("num -> char : %c\n", num, ch, num);
+}
+
+void printB(){
+	int num = 65;
+	double pie = 3.14;
+
+	printf("num : %d\n", num);
+	printf("num : %5d\n", num);
+	printf("num : %05d\n", num);
+	printf("num : %-5d\n", num);
+
+	printf("\n");
+
+	printf("num : %f\n", pie);
+	printf("num : %10f\n", pie);
+	printf("num : %.2f\n", pie);
+	printf("num : %010.2f\n", pie);
+	printf("num : %-10.2f\n", pie);
+}
+
+int main(){
+	printA();
+
+	printf("\n");
+	
+	printB();
 
 	return 0;
 }
